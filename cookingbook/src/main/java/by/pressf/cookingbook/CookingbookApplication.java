17571c5bf94f4191f0dac1af7777c330d715a76a
PressF <artemsurmenok@gmail.com>
2026-04-04 13:46:04 +0300

    Подключение необходимых модулей и зависимостей для начала написания кода на серверной части

---
+++ cookingbook/src/main/java/by/pressf/cookingbook/CookingbookApplication.java
@@ -0,0 +1,13 @@
+package by.pressf.cookingbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CookingbookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CookingbookApplication.class, args);
+	}
+
+}
---
+++ cookingbook/src/test/java/by/pressf/cookingbook/CookingbookApplicationTests.java
@@ -0,0 +1,13 @@
+package by.pressf.cookingbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CookingbookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
